6cb0c5ad941bceb28f8998567c2c27f606ba4035
Indra Lukmana <indralukmana@live.com>
2020-06-28 22:58:21 +0700

    📦 NEW: Add user details on profile screen

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -6,11 +6,18 @@ import { AuthActionTypes } from "../types";
 import { AuthContext } from "../Context/AuthenticationContext";
 
 export default function ProfileScreen() {
-  const { dispatchAuth } = React.useContext(AuthContext);
+  const { dispatchAuth, authState } = React.useContext(AuthContext);
 
   return (
     <View style={tailwind("flex-1 justify-center items-center")}>
-      <Text style={tailwind("font-bold mb-20")}>Profile</Text>
+      <Text style={tailwind("font-bold")}>Profile</Text>
+      <Text style={tailwind("mt-10")}>
+        Name: {authState.userData?.displayName}
+      </Text>
+      <Text style={tailwind("mt-3")}>Email: {authState.userData?.email}</Text>
+      <Text style={tailwind("mt-3 mb-5")}>
+        Country: {authState.userData?.country}
+      </Text>
       <Button
         title="log out"
         color={getColor("red-500")}
