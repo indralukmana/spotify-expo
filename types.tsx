9c4fb43018806aad76d1e4df430be292a72a7d36
Indra Lukmana <indralukmana@live.com>
2020-06-28 19:35:12 +0700

    📦 NEW: Add featured playlist type

--- types.tsx
+++ types.tsx
@@ -31,3 +31,40 @@ export type AuthenticationContextType = {
   authState: AuthState;
   dispatchAuth: React.Dispatch<AuthAction>;
 };
+
+export type Image = {
+  height: null | number;
+  url: string;
+  width: null | number;
+};
+
+export type FeaturedPlaylist = {
+  collaborative: false;
+  description: string;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  owner: {
+    display_name: string;
+    external_urls: {
+      spotify: string;
+    };
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  primary_color: null;
+  public: null;
+  snapshot_id: string;
+  tracks: {
+    href: string;
+    total: number;
+  };
+  type: string;
+  uri: string;
+};
