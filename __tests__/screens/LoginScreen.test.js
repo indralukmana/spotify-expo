b51612ce983b6f82a8fff5a217eee8e87003f1d0
Indra Lukmana <indralukmana@live.com>
2020-06-29 09:54:33 +0700

    📦 NEW: Add login screen unit test

---
+++ __tests__/screens/LoginScreen.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import LoginScreen from "../../screens/LoginScreen";
+
+test("Smoke test", async () => {
+  const { baseElement } = render(<LoginScreen />);
+  expect(baseElement.children.length).toBe(1);
+});
+
+test("login button rendered", async () => {
+  const { getByText } = render(<LoginScreen />);
+  getByText("Log In");
+});
